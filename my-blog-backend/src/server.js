903ff2015bede2f9890ef89b676cb79892d653ad
Mick Wang <wang.mick12@gmail.com>
2024-02-03 23:21:36 -0500

    update site build

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,8 +1,21 @@
 import express from 'express';
+import path from 'path';
 import { db, connectToDb } from './db.js';
+import { fileURLToPath } from 'url';
+import 'dotenv/config';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../build')));
+
+//when browser sends request to server, that isn't an api route, server will send back index.html
+app.get(/^(?!\/api).+/, (req, res) => {
+    res.sendFile(path.join(__dirname, '../build/index.html'));
+});
+
 
 app.get('/api/articles/:name', async (req, res) => {
     const { name } = req.params;
@@ -47,9 +60,11 @@ app.post('/api/articles/:name/comments', async (req, res) => {
     }
 });
 
+const PORT = process.env.PORT || 8000;
+
 connectToDb(() => {
     console.log('Successfully connected to database!');
-    app.listen(8000, () => {
-        console.log('Server is listening on port 8000');
+    app.listen(PORT, () => {
+        console.log('Server is listening on port '+ PORT);
     });
 })
--- myblog/src/pages/ArticlePage.js
+++ myblog/src/pages/ArticlePage.js
@@ -1,53 +1,49 @@
-import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
-import NotFoundPage from './NotFoundPage';
-import CommentsList from '../components/CommentsList';
-import AddCommentForm from '../components/AddCommentForm';
-import articles from './article-content';
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import NotFoundPage from "./NotFoundPage";
+import articles from "./article-content";
 
 const ArticlePage = () => {
-    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
-    const { articleId } = useParams();
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0 });
+  const { articleId } = useParams();
 
-    useEffect(() => {
-        const loadArticleInfo = async () => {
-            const response = await axios.get(`/api/articles/${articleId}`);
-            const newArticleInfo = response.data;
-            setArticleInfo(newArticleInfo);
-        }
+  useEffect(() => {
+    const loadArticleInfo = async () => {
+      const response = await axios.get(`/api/articles/${articleId}`);
+      const newArticleInfo = response.data;
+      setArticleInfo(newArticleInfo);
+    };
 
-        loadArticleInfo();
-    }, []);
+    loadArticleInfo();
+  }, []);
 
-    const article = articles.find(article => article.name === articleId);
+  const article = articles.find((article) => article.name === articleId);
 
-    const addUpvote = async () => {
-        const response = await axios.put(`/api/articles/${articleId}/upvote`);
-        const updatedArticle = response.data;
-        setArticleInfo(updatedArticle);
-    }
+  const addUpvote = async () => {
+    const response = await axios.put(`/api/articles/${articleId}/upvote`, null);
+    const { upvotes } = response.data;
+    setArticleInfo(() => ({
+      upvotes: upvotes,
+    }));
+  };
 
-    if (!article) {
-        return <NotFoundPage />
-    }
+  if (!article) {
+    return <NotFoundPage />;
+  }
 
-    return (
-        <>
-        <h1>{article.title}</h1>
-        <div className="upvotes-section">
-            <button onClick={addUpvote}>Upvote</button>
-            <p>This article has {articleInfo.upvotes} upvote(s)</p>
-        </div>
-        {article.content.map((paragraph, i) => (
-            <p key={i}>{paragraph}</p>
-        ))}
-        <AddCommentForm
-            articleName={articleId}
-            onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)} />
-        <CommentsList comments={articleInfo.comments} />
-        </>
-    );
-}
+  return (
+    <>
+      <h1>{article.title}</h1>
+      <div className="upvotes-section">
+        <button onClick={addUpvote}>Upvote</button>
+        <p>This article has {articleInfo.upvotes} upvote(s)</p>
+      </div>
+      {article.content.map((paragraph, i) => (
+        <p key={i}>{paragraph}</p>
+      ))}
+    </>
+  );
+};
 
 export default ArticlePage;
